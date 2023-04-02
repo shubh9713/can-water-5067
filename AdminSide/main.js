d3ceccb80bcf2e1847e8d0221b55dd80b335740f
unknown <murmuparmeshwar05@gmail.com>
2023-04-02 11:54:45 +0530

    M

--- AdminSide/main.js
+++ AdminSide/main.js
@@ -1,172 +1,330 @@
-let brand=document.getElementById("brand");
-let image=document.getElementById("image");
-let title=document.getElementById("title");
-let price=document.getElementById("price");
-let category= document.getElementById("category")
-let description= document.getElementById("description")
-let country= document.getElementById("country")
+let url = "http://localhost:3000/grocery";
 
-let addatabutton=document.getElementById("addProducts");
-let tbody=document.querySelector("tbody");
+let tbody = document.querySelector("tbody");
+let productImage = document.getElementById("productImage");
+let productPrice = document.getElementById("productPrice");
+let updatProductBtn = document.getElementById("updatProductBtn");
+let totalitems = document.getElementById("totalitems");
+let addBtn = document.getElementById("addBtn");
+let productDiv = document.getElementById("productDiv");
 
+let addProductsBtn = document.getElementById("addProducts");
+let image = document.getElementById("image");
+let brand = document.getElementById("brand");
+let title = document.getElementById('title');
+let price = document.getElementById("price");
+let category = document.getElementById("category");
+let description = document.getElementById("description");
+let country = document.getElementById("country");
+let seasonal = document.getElementById("seasonal");
+let quantity = document.getElementById("quantity");
+let data = document.getElementById("data");
 
+let pagination = document.getElementById("pagination");
 
-/////////////////////////////////////////
+// window.addEventListener("load", function(){
+    fetchGroceryData();
+// })
+// let totalDatalen = totalitems.innerText
+// console.log(totalDatalen, "totla", typeof(totalDatalen))
+function fetchGroceryData(pageNumber)
+{
+    anger()
+    fetch(`${url}?_limit=6&_page=${pageNumber}`)
+    .then((res) => {
+        let totalItems = 40;
+        // console.log(res.length)
+        createPagination(totalItems);
 
-let updateimg= document.getElementById("updateimg")
-let updateprice= document.getElementById("updateprice")
-let updatebtn= document.getElementById("updateProducts")
-
-async function fetchdata(){
-
-    
-
-    let res=await fetch("http://localhost:3000/grocery")
-    let data=await res.json()
-    console.log(data)
-    data.forEach( (e,index)=> {
-        
-       tbody.append( append1(e.brand,e.price,e.image,e.title,e.id,e.category,e.country, e.description,index+1))
-    });
-}
-fetchdata()
-
-addatabutton.addEventListener("click",()=>{
-    
-    let data={
-        brand:brand.value,
-        image:image.value,
-        title:title.value,
-        price:price.value,
-        category: category.value,
-        description: description.value,
-        country: country.value,   
-    }
-    addata(data)
-})
-
-async function addata(x){
-
-    let res=await fetch("http://localhost:3000/grocery",{
-        method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(x)
+        return res.json()
     })
-    let data=await res.json()
-    console.log(data)
-}
- 
-let url= "http://localhost:3000/grocery"
-
-function append1(brand,price,img,title,id,category,description,country,index){
-
-    let tr=document.createElement("tr");
-    tr.setAttribute("data-id",id)
-    let brand1=document.createElement("td");
-    let price1=document.createElement("td");
-    let title1=document.createElement("td");
-    let image1=document.createElement("img");
-    let category1= document.createElement("td")
-    let country1= document.createElement("td")
-    let description1= document.createElement("td")
-    
-
-    image1.className="image";
-    brand1.innerText=brand;
-    price1.innerText=price;
-    title1.innerText=title;
-    category1.innerText= category;
-    country1.innerText= country;
-    description1.innerText= description;
-
-
-    image1.src=img;
-    let edit=document.createElement("button")
-    let remove=document.createElement("button")
-    remove.innerText="Remove Item";
-
-    let Edit= document.createElement("button")
-    remove.addEventListener("click",()=>{
-        deleteitem(id)
-    })
-
-
-    Edit.innerText="Click Here To Edit"
-    // Edit.addEventListener("click",()=>{
-
-    //     localStorage.setItem("id",index)   
-     Edit.addEventListener("click",(e)=>{
-         localStorage.setItem("id", index)
-          updateProducts(index);
+    .then((data) => {
+        console.log(data)
+        appendData(data);
        
-     })
+    })
+
     
-   
-    tr.append(image1,brand1,title1, price1,category1,country1,description1,remove,Edit)
-     return tr
-    
+}
+
+ function anger(){
+    fetch(url)
+    .then((res) => res.json())
+    .then((data)=>{
+        totalitems.innerText = data.length;
+    })
+ }
+
+function createPagination(totalItems)
+{
+    let totalBuutons = Math.ceil(totalItems/6);
+    pagination.innerHTML = "";
+    for(let i=1; i<=totalBuutons; i++)
+    {
+        pagination.append(getButton(i));
+    }
+}
+
+function getButton(i){
+    let btn = document.createElement("button");
+    btn.setAttribute("class", "paginateButton");
+    btn.setAttribute("data-id", i);
+    btn.innerText = i;
+
+    btn.addEventListener("click", function(e){
+        fetchGroceryData(e.target.dataset.id)
+    })
+
+    return btn
 }
 
 
 
-///////////////////////////////////////////// function for update product
+function appendData(data)
+{
+    tbody.innerHTML = "";
+    // totalitems.innerText = ele.length;
+    data.forEach((ele) => {
+        tbody.append(getCard(ele.image, ele.title, ele.brand, ele.price, ele.category, ele.description, ele.country, ele.id));
+        
+    });
 
-updatebtn.addEventListener("click",function(){
-    let EditId = JSON.parse(localStorage.getItem("id"));
-    console.log(EditId);
-    updateProducts(EditId);
+    // for(let i=ele.length-1; i>=0; i--)
+    // {
+    //     tbody.append(getCard(ele[i].image, ele[i].title, ele[i].brand, ele[i].price, ele[i].category, ele[i].description, ele[i].country, ele[i].id));
+
+    // }
+}
+
+
+
+function getCard(productImg, title, brand, price, category, description,country, id){
+
+    let tr = document.createElement("tr");
+
+
+    let td1 = document.createElement("td");
+    let image = document.createElement('img')
+    image.setAttribute("src", productImg);
+    td1.append(image);
+
+    let td2 = document.createElement("td");
+    let p2 = document.createElement("p")
+    p2.innerText = title;
+    td2.append(p2);
+
+    let td3 = document.createElement("td");
+    let p3 = document.createElement("p")
+    p3.innerText = brand;
+    td3.append(p3);
+
+    let td4 = document.createElement("td");
+    let p4 = document.createElement("p")
+    p4.innerText = price;
+    td4.append(p4)
+
+    let td5 = document.createElement("td");
+    let p5 = document.createElement("p")
+    p5.innerText =category;
+    td5.append(p5)
+
+    let td6 = document.createElement("td");
+    let p6 = document.createElement("p")
+    p6.innerText = description;
+    td6.append(p6)
+
+    let td7 = document.createElement("td");
+    let p7 = document.createElement("p");
+    p7.innerText = country;
+    td7.append(p7);
+
+    // let td8 = document.createElement("td");
+    // let updateProductBtn = document.createElement("button");
+    // updateProductBtn.innerText = "ADD PRODUCT"
+    // td8.append(updateProductBtn);
+   
+
+    let td9 = document.createElement("td");
+    let removeBtn = document.createElement("button");
+    removeBtn.setAttribute("id", "removeBtn")
+    removeBtn.innerText = "DELETE"
+    td9.append(removeBtn);
+
+    removeBtn.addEventListener("click", function(){
+        // console.log(id)
+        deleteCategoryItem(id);
+    })
+
+    let td10 = document.createElement("td");
+    let editBtn = document.createElement("button");
+    editBtn.setAttribute("id", "editBtn")
+    editBtn.innerText = "EDIT";
+    td10.append(editBtn);
+
+    editBtn.addEventListener("click", function(){
+        productDiv.style.display = "block";
+        // data.style.width = "100%"
+        // data.style.width= "0%";
+        data.style.marginLeft = "50px";
+        editProductDetails(id);
+    })
+
+    tr.append(td1, td2, td3, td4, td5, td6, td7,  td9, td10);
+
+    return tr
+
+    
+}
+
+
+//Deleting item from grocery
+function deleteCategoryItem(id)
+{
+    fetch(`${url}/${id}`, {
+        method:"DELETE"
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        alert("Item Deleted Successfully");
+    })
+}
+
+
+//editing items in grocery
+
+function editProductDetails(id){
+    updatProductBtn.addEventListener("click", function(){
+        update(id);
+    })
+}
+
+function update(id)
+{
+    let obj = {
+        image: productImage.value,
+        price: productPrice.value
+    }
+    fetch(`${url}/${id}`, {
+        method: "PATCH",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(obj)
+        
+
+    })
+    .then((res) => res.json())
+    .then((data) =>{
+        console.log(data);
+        alert("Item Updated Successfully");
+    })
+}
+
+
+addBtn.addEventListener("click", function(){
+    productDiv.style.display = "block";
+    data.style.marginLeft = "50px";
 })
 
 
-function updateProducts(getproduct){
-    let obj={
-        image: updateimg.value,
-        price: updateprice.value
+// ADDING ITEMS
+
+addProductsBtn.addEventListener("click", function(){
+    if(image.value == "")
+    {
+        alert("Image cannot be empty");
     }
-    console.log(obj);
-    fetch(`${url}/${getproduct}`,{
-        method:"PATCH",
+    else if(brand.value == "")
+    {
+        alert("Brand canno be empty")
+    }
+    else if(title.value == "")
+    {
+        alert("Title cannot be empty");
+    }
+    else if(price.value == "")
+    {
+        alert("Price cannot be empty")
+    }
+    else if(category.value == "")
+    {
+        alert("Category cannot be empty")
+    }
+    else if(description.value == "")
+    {
+        alert("Description cannot bee empty")
+    }
+    else if(country.value == "")
+    {
+        alert("Country cannot be empty");
+    }
+    else if(seasonal.value == "")
+    {
+        alert("seasonal cannot be empty");
+    }
+    else if(quantity.value == "")
+    {
+        alert("quantity cannot be empty");
+    }
+    else{
+        let obj = {
+            image : image.value,
+            title: title.value,
+            price :price.value,
+            category:category.value,
+            description:description.value,
+            brand:brand.value,
+            seasonal:seasonal.value,
+            country: country.value,
+            quantity:quantity.value
+
+        }
+
+        addProductToData(obj);
+    }
+})
+
+function addProductToData(obj)
+{
+    fetch(url, {
+        method: "POST",
         headers:{
-       "Content-Type":"application/json"
-       },
-       body:JSON.stringify(obj)
-                
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(obj)
     })
-            
-  .then((res)=> res.json())
-  .then((data)=>{
-    console.log("updating",data);
-    fetchdata();
-  })
-  localStorage.removeItem("id")
-  .catch((err)=> console.log(err))
-}
-
-
-
-
-
-
- function deleteitem(id){
-    fetch(`http://localhost:3000/grocery/${id}`,{
-        method:"DELETE",
-        headers:{ "Content-Type":"application/json"}
+    .then((res) => res.json())
+    .then((data) =>{
+        console.log("Posted Successfully", data)
+        reverseData();
     })
 }
 
+function reverseData(){
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) =>{
+    //   newlyAddedData(data);
+    alert("Product added Successfully");
+    })
+}
 
-// async function edit1(id,data1){
-//    let res=await fetch(`http://localhost:3000/grocery/${id}`,{
-//         method:"PATCH",
-//         headers:{ "Content-Type":"application/json"},
-//         body:JSON.stringify(data1)
+// function newlyAddedData(ele){
+//     tbody.innerHTML = "";
+//     for(let i=ele.length-1; i>=0; i--)
+//     {
+//         tbody.append(getCard(ele[i].image, ele[i].title, ele[i].brand, ele[i].price, ele[i].category, ele[i].description, ele[i].country, ele[i].id));
 
-//     })
-//     let data=await res.json()
-//     console.log(data)
+//     }
 // }
+
+
+
+
+
+
+
 
 
 
